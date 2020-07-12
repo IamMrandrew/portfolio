17a255f6c8f182a294fa60f04aa9337c34c51ea9
IamMrandrew <andrewliyanlap000824@gmail.com>
2020-07-13 01:42:29 +0800

    Updated index.html, index.css, index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
-console.log("Hello World");
+function toggleNavList(){
+    const burgerToggle = document.querySelector('.burgerToggle');
+    const navlist = document.querySelector('.nav-list');
+
+    burgerToggle.addEventListener('click', function() {
+        navlist.classList.toggle('nav-active');
+        burgerToggle.classList.toggle('burger-active')
+    });
+}
+toggleNavList();
+
+function scrollTo(element) {
+    window.scroll({
+      behavior: 'smooth',
+      left: 0,
+      top: element.offsetTop
+    });
+  }
+  
+  document.getElementById("designTab").addEventListener('click', () => {
+    scrollTo(document.getElementById("design"));
+  });
+
+
