6503f97a8f0c5bd09e23b53481867542be22ac8c
IamMrandrew <andrewliyanlap000824@gmail.com>
2020-07-13 03:23:44 +0800

    Updated index.html, Added about.html

--- js/index.js
+++ js/index.js
@@ -1,13 +1,12 @@
-function toggleNavList(){
-    const burgerToggle = document.querySelector('.burgerToggle');
-    const navlist = document.querySelector('.nav-list');
 
-    burgerToggle.addEventListener('click', function() {
-        navlist.classList.toggle('nav-active');
-        burgerToggle.classList.toggle('burger-active')
-    });
-}
-toggleNavList();
+const burgerToggle = document.querySelector('.burgerToggle');
+const navlist = document.querySelector('.nav-list');
+
+burgerToggle.addEventListener('click', function() {
+    navlist.classList.toggle('nav-active');
+    burgerToggle.classList.toggle('burger-active')
+});
+
 
 function scrollTo(element) {
     window.scroll({
