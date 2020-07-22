c7c7ce8fc76a2ce7f8d779f3c24af6aefd403ebf
IamMrandrew <andrewliyanlap000824@gmail.com>
2020-07-22 20:58:49 +0800

    style: redesign skills cards on mobile ( about.html )

--- js/about.js
+++ js/about.js
@@ -2,8 +2,16 @@
 function scrollAppear() {
     let contact = document.querySelector(".form-wrapper");
     let skill = document.querySelector(".skill-wrapper");
+
+    let skill1 = document.querySelector(".skillContainer-mobile-1");
+    let skill2 = document.querySelector(".skillContainer-mobile-2");
+    let skill3 = document.querySelector(".skillContainer-mobile-3");
+
     let contactSession = contact.getBoundingClientRect().top;
     let skillSession = skill.getBoundingClientRect().top;
+    let skill1Session = skill1.getBoundingClientRect().top;
+    let skill2Session = skill2.getBoundingClientRect().top;
+    let skill3Session = skill3.getBoundingClientRect().top;
     let screenPosition = window.innerHeight / 1.4;
 
     if (screenPosition > contactSession) {
@@ -11,6 +19,15 @@ function scrollAppear() {
     }
     if (screenPosition > skillSession) {
         skill.classList.add("itemAppear");
+    }
+    if (screenPosition > skill1Session) {
+        skill1.classList.add("itemAppear");
+    }
+    if (screenPosition > skill2Session) {
+        skill2.classList.add("itemAppear");
+    }
+    if (screenPosition > skill3Session) {
+        skill3.classList.add("itemAppear");
     }
 }
 
