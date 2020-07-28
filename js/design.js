987419a555977a2c2e97685ec4c52618a491a06d
IamMrandrew <andrewliyanlap000824@gmail.com>
2020-07-28 21:41:39 +0800

    style: updated hau-tak-church.html

--- js/design.js
+++ js/design.js
@@ -60,63 +60,6 @@ window.addEventListener("scroll", scrollAppear);
 
 // hau-tak-church
 
-// let sliders = document.querySelectorAll('.glide');
-
-//     const config = {
-//         type: "slider",
-//         perView: 1.1,
-//         gap: 20,
-//         keyboard: true,
-//         rewind: false,
-//         bound: true,
-//         animationDuration: 950,
-//         breakpoints: {
-//             1140: {
-//                 perView: 1.3
-//             },
-//             768: {
-//                 perView: 1
-//             }
-//         }
-//     }
-
-//     new Glide(".glide1", config).mount();
-//     new Glide(".glide2", {
-//         type: "slider",
-//         perView: 2.7,
-//         gap: 5,
-//         keyboard: true,
-//         rewind: false,
-//         bound: true,
-//         animationDuration: 950,
-//         breakpoints: {
-//             1140: {
-//                 perView: 2.9
-//             },
-//             768: {
-//                 perView: 2.2
-//             },
-//             425: {
-//                 perView: 1.3
-//             }
-//         }
-//     }).mount();
-
-// let glides = document.querySelectorAll(".glide");
-// let arrows = document.querySelectorAll(".glide__arrow");
-
-//     for(let i = 0; i < glides.length; i++){
-//         glides[i].addEventListener('mouseover', function(){
-//             let secondArrow = i*2+1;
-//             arrows[i*2].classList.add("arrowsAppear");
-//             arrows[secondArrow].classList.add("arrowsAppear");
-//             setTimeout(function() {
-//                 arrows[i*2].classList.remove("arrowsAppear");
-//                 arrows[secondArrow].classList.remove("arrowsAppear");
-//             }, 4000);
-//         })
-//     }
-
 new Glider(document.querySelector('.glider'), {
     // slidesToShow: 'auto',
     // itemWidth: 320,
@@ -142,7 +85,7 @@ new Glider(document.querySelector('.glider'), {
         }, {
             breakpoint: 1700,
             settings: {
-                slidesToShow: 2.3,
+                slidesToShow: 2.1,
             }
         }
     ]
@@ -163,7 +106,7 @@ new Glider(document.querySelector('.glider2'), {
         {
             breakpoint: 768,
             settings: {
-                slidesToShow: 1.8,
+                slidesToShow: 1.9,
             }
         }, {
             breakpoint: 1440,
@@ -174,6 +117,11 @@ new Glider(document.querySelector('.glider2'), {
             breakpoint: 1700,
             settings: {
                 slidesToShow: 3.4,
+            }
+        }, {
+            breakpoint: 2100,
+            settings: {
+                slidesToShow: 4,
             }
         }
     ]
