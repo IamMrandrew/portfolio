d16404be8609044c260680cb499928b1eeca0215
IamMrandrew <andrewliyanlap000824@gmail.com>
2021-01-17 18:52:50 +0800

    feat: responsive animated nav bar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,21 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Nav = () => {
+  const [burger, setBurger] = useState(false);
+
+  useEffect(() => {
+    if (burger) {
+      document.querySelector("body").classList.add("scroll-lock");
+    } else {
+      document.querySelector("body").classList.remove("scroll-lock");
+    }
+  }, [burger]);
+
   return (
-    <nav className="nav">
-      <div className="custom-container">
-        <div className="nav-brand">andrew li</div>
-        <ul className="nav-list">
-          <li className="nav-item">Works</li>
-          <li className="nav-item">Profile</li>
-        </ul>
-        <div className="burger">
-          <div className="bar bar1"></div>
-          <div className="bar bar2"></div>
-          <div className="bar bar3"></div>
+    <>
+      <nav className="nav">
+        <div className="custom-container">
+          <div className="nav-brand">andrew li</div>
+          <ul className={burger ? "nav-list active" : "nav-list"}>
+            <li className="nav-item burger-title">Explore More</li>
+            <li className="nav-item">Works</li>
+            <li className="nav-item">Profile</li>
+          </ul>
+          <div
+            className={burger ? "burger active " : "burger"}
+            onClick={() => {
+              setBurger(!burger);
+            }}
+          >
+            <div className="bar bar1"></div>
+            <div className="bar bar2"></div>
+            <div className="bar bar3"></div>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+      <div className={burger ? "pusher active" : "pusher"}></div>
+      <div className={burger ? "faded active" : "faded"}></div>
+    </>
   );
 };
 
