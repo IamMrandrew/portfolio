29e6dc99894d281e33964410a0b1baf728337a24
IamMrandrew <andrewliyanlap000824@gmail.com>
2021-01-20 02:20:52 +0800

    feat: text transition

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,13 @@
 import React from "react";
-import { motion } from "framer-motion";
 import Hero from "./Hero";
 import Works from "./Works";
 
 const Home = () => {
   return (
-    <motion.div
-      exit={{ opacity: 0 }}
-      transition={{ duration: 0.5, ease: [0.6, 0.01, -0.05, 0.9] }}
-      className="home"
-    >
+    <div className="home">
       <Hero />
       <Works />
-    </motion.div>
+    </div>
   );
 };
 
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,20 +1,15 @@
 import React from "react";
-import { motion } from "framer-motion";
 import About from "./About";
 import Contact from "./Contact";
 import Skills from "./Skills";
 
 const Profile = () => {
   return (
-    <motion.div
-      exit={{ opacity: 0 }}
-      transition={{ duration: 0.5, ease: [0.6, 0.01, -0.05, 0.9] }}
-      className="profile"
-    >
+    <div className="profile">
       <About />
       <Skills />
       <Contact />
-    </motion.div>
+    </div>
   );
 };
 export default Profile;
