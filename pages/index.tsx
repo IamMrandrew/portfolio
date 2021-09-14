900c507a6725575ccff5ed835241675717bfdf74
IamMrandrew <andrewliyanlap000824@gmail.com>
2021-09-14 20:34:32 +0800

    feat: hero section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
 import Head from "next/head";
+import styled from "styled-components";
+import { StyledContainer } from "../styles/Container";
 import Works from "../components/Works";
 import { Client } from "@notionhq/client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { MEDIA_BREAK } from "../styles/GlobalStyle";
 
 type Props = {
   results: any;
@@ -12,11 +15,69 @@ export default function Home({ results }: Props) {
     console.log(results);
   }, []);
 
+  const [text, setText] = useState("");
+
+  useEffect(() => {
+    const texts = [
+      "creating beautiful things",
+      "solving problems with my own creation",
+      "exploring interesting things",
+    ];
+    let index = 0;
+    let letterIndex = 0;
+    let currentText = "";
+
+    const typing = () => {
+      index %= texts.length;
+
+      currentText = texts[index];
+
+      setText(currentText.slice(0, ++letterIndex));
+
+      if (currentText.slice(0, letterIndex).length === currentText.length) {
+        index++;
+        letterIndex = 0;
+      }
+    };
+
+    const typewriter = setInterval(typing, 400);
+
+    return () => {
+      clearInterval(typewriter);
+    };
+  }, []);
+
   return (
     <div>
       <Head>
         <title>Create Next App</title>
       </Head>
+      <Hero>
+        <StyledContainer>
+          <TextWrapper>
+            <OverflowWrapper>
+              <Title>
+                Hello, I am andrew <br></br> a{" "}
+                <Highlighting>
+                  Computer&nbsp;
+                  <Highlighter></Highlighter>
+                </Highlighting>
+                <Highlighting>
+                  Science&nbsp;
+                  <Highlighter></Highlighter>
+                </Highlighting>
+                <Highlighting>
+                  student
+                  <Highlighter></Highlighter>
+                </Highlighting>
+              </Title>
+            </OverflowWrapper>
+            <OverflowWrapper>
+              <Subtitle>I love {text}</Subtitle>
+            </OverflowWrapper>
+          </TextWrapper>
+        </StyledContainer>
+      </Hero>
       <Works works={results} />
     </div>
   );
@@ -39,3 +100,87 @@ export async function getStaticProps() {
     revalidate: 10,
   };
 }
+
+const Hero = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 450px;
+`;
+
+const OverflowWrapper = styled.div`
+  overflow: hidden;
+`;
+
+const TextWrapper = styled.div`
+  width: 100%;
+`;
+
+const Title = styled.h1`
+  color: ${({ theme }) => theme.text.primary};
+
+  @media screen and (max-width: ${MEDIA_BREAK.md}) {
+    font-size: 30px;
+  }
+`;
+
+const Highlighting = styled.span`
+  position: relative;
+  display: inline-block;
+`;
+
+const Highlighter = styled.div`
+  position: absolute;
+  z-index: -1;
+  top: 40px;
+  left: 15px;
+  width: 100%;
+  height: 35px;
+  background-color: ${({ theme }) => theme.bg.highlighted};
+  opacity: 0.9;
+
+  @media screen and (max-width: ${MEDIA_BREAK.md}) {
+    top: 20px;
+    left: 8px;
+    width: 100%;
+    height: 18px;
+    background-color: $highlighted;
+    opacity: 0.9;
+  }
+`;
+
+const Subtitle = styled.p`
+  font-size: 30px;
+  font-weight: 500;
+  color: ${({ theme }) => theme.text.tinted};
+  letter-spacing: 1px;
+  line-height: 1.3;
+
+  @media screen and (max-width: ${MEDIA_BREAK.md}) {
+    font-size: 18px;
+  }
+  &::after {
+    content: "";
+    width: 5px;
+    height: 4px;
+    border-right: 3px solid ${({ theme }) => theme.text.tinted};
+    margin: 8px;
+    animation: blink 0.5s step-end infinite;
+
+    @media screen and (max-width: ${MEDIA_BREAK.md}) {
+      border-right: 2px solid ${({ theme }) => theme.text.tinted};
+    }
+
+    @keyframes blink {
+      0% {
+        opacity: 1;
+      }
+      50% {
+        opacity: 0;
+      }
+      100% {
+        opacity: 1;
+      }
+    }
+  }
+`;
